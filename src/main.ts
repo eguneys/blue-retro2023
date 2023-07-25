ced089c02ff35aaa79ab567a432f9128ba328473
Emre Guneyler <eguneys@gmail.com>
2023-07-25 21:44:56 +0300

    hello update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
+import { App } from './app'
+import Content from './content'
+import Graphics from './graphics'
+import Scene1 from './scene1'
 
-function app(element: HTMLElement) {
-  console.log(element)
+function start(element: HTMLElement) {
 
+  let graphics = Graphics.make()
+  let scene = Scene1.make()
+
+
+  App.run({
+    on_update() {
+      scene.update()
+    },
+    on_render() {
+      scene.draw(graphics)
+    }
+  })
+
+}
+
+async function app(element: HTMLElement) {
+  await Content.load()
+  start(element)
 }
 
 app(document.getElementById('app')!)
